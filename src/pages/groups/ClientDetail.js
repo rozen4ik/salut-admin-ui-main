ac8dbf27f56046baaa290785ace7a1faba3fde3d
Mikhail Rozenberg <ro3enn@yandex.ru>
2023-03-07 16:46:27 +0300

    Подготовка для карточки клиента

--- src/pages/groups/ClientDetail.js
+++ src/pages/groups/ClientDetail.js
@@ -1,11 +1,23 @@
 import { Box, Grid } from '@mui/material';
 import MainCard from '../../components/MainCard';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import expressController from '../../api/ExpressController';
 
 const ClientDetail = () => {
     const params = useParams();
     const id_acc = params.id_acc;
+    let client = useState([]);
+
+    useEffect(() => {
+        loadGetClientDetail();
+    });
+
+    const loadGetClientDetail = async () => {
+        client = await expressController.getDetailClient(id_acc);
+        console.log(client[0]);
+    };
+
     return (
         <MainCard title="Карточка клиента">
             <Box>
@@ -14,7 +26,7 @@ const ClientDetail = () => {
                         <h4>Основная информация:</h4>
                     </Grid>
                     <Grid item xs={12}>
-                        <h4>ФИО</h4>
+                        <h4>ФИО: {client[0]}</h4>
                     </Grid>
                     <Grid item xs={12}>
                         <h4>Мосрег</h4>
